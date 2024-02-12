8798be831de9f4d261136a49e904c2bf04e4d8f9
paresh <routpareshkumar737@gmail.com>
2024-02-12 22:16:17 +0530

    jsx,components

--- video-1/App.js
+++ video-1/App.js
@@ -2,13 +2,31 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+//! const heading = <h1>My Name is paresh🧨</h1>
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(heading);
 
-const parent = React.createElement('div',{id:'parent'},
-React.createElement('div',{id:'child'},
-[React.createElement('h1',{},'My name is Bapu'),
-React.createElement('h2',{},'My name is paresh'),React.createElement('h1',{},'My name is chintu'),
-React.createElement('h2',{},'My name is amar')]))
+//? const parent = React.createElement('div',{id:'parent'},
+// React.createElement('div',{id:'child'},
+// [React.createElement('h1',{},'My name is Bapu'),
+// React.createElement('h2',{},'My name is paresh'),React.createElement('h1',{},'My name is chintu'),
+// *React.createElement('h2',{},'My name is amar')]))
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
-root.render(parent)
+// root.render(parent)
+
+function Header(){
+    return(
+        <>
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Contact Us</li>
+            </ul>
+        </>
+    )
+}
+
+root.render(<Header/>);
+
