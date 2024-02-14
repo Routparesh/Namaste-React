cd8832a697ad2ec0fb8edb95c6a3cdaeda6b71a2
paresh <routpareshkumar737@gmail.com>
2024-02-15 01:15:48 +0530

    create resturant card component

--- food-order-app/src/App.jsx
+++ food-order-app/src/App.jsx
@@ -1,10 +1,12 @@
 
 import Header from "./components/Header/Header"
+import ResturantCard from "./components/ResturantCard/ResturantCard"
 function AppLayout() {
   return (
   
       <div className="app">
          <Header/>
+         <ResturantCard/>
       </div>
   )
 }
--- food-order-app/src/components/Header/Header.jsx
+++ food-order-app/src/components/Header/Header.jsx
@@ -1,15 +1,23 @@
+import './Header.css'
+
 const Header = ()=>{
 
     return(
         <div className="header">
             <div className="logo">
-                <img src="" alt="" />
+                <img src="src\assets\bhojan_bazar-removebg-preview.png" alt="" />
             </div>
-            <ul>
-                <li>Home</li>
-                <li>About Us</li>
-                <li>Contact Us</li>
-            </ul>
+
+            <div className='nav-Item'>
+               <ul>
+                   <li>Home</li>
+                   <li>About Us</li>
+                   <li>Contact Us</li>
+                </ul>
+            </div>
+            <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" className="bi bi-cart" viewBox="0 0 16 16">
+  <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5M3.102 4l1.313 7h8.17l1.313-7zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4m7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4m-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2m7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2"/>
+</svg>
         </div>
     )
     
