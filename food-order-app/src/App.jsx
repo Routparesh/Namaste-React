ee8eddecf79b38f2aebdf776c3b25f71994d65c4
paresh <routpareshkumar737@gmail.com>
2024-02-14 23:53:19 +0530

    start creating a food ordering app

---
+++ food-order-app/src/App.jsx
@@ -0,0 +1,12 @@
+
+import Header from "./components/Header/Header"
+function AppLayout() {
+  return (
+  
+      <div className="app">
+         <Header/>
+      </div>
+  )
+}
+
+export default AppLayout
---
+++ food-order-app/src/components/Header/Header.jsx
@@ -0,0 +1,18 @@
+const Header = ()=>{
+
+    return(
+        <div className="header">
+            <div className="logo">
+                <img src="" alt="" />
+            </div>
+            <ul>
+                <li>Home</li>
+                <li>About Us</li>
+                <li>Contact Us</li>
+            </ul>
+        </div>
+    )
+    
+}
+
+export default Header;
